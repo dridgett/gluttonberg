7e0318b7f24b4a594b67ad058fb63eef7580f03d
Luke Matthew Sutton <me@lukematthewsutton.com>
2010-11-10 20:15:11 +1030

    Generated rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gluttonberg::Application.config.secret_token = 'b734c148881e2ed5ba0b73ef783f4bcfe322e6c67be61325e68e845052e95a6ab734be20693ec5bd7d5e7a1bab21db83e7542ff79f6822e281c611138cf579b1'
