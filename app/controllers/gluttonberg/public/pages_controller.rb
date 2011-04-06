6977d62093ae0633565b6acb3f943c4d23a1066f
Luke Matthew Sutton <me@lukematthewsutton.com>
2011-04-06 22:24:34 +0930

    Basic page rendering, with temporary template selection code.

---
+++ app/controllers/gluttonberg/public/pages_controller.rb
@@ -0,0 +1,27 @@
+module Gluttonberg
+  module Public
+    class PagesController < ActionController::Base
+      layout "public"
+      
+      before_filter :retrieve_page_and_locale
+
+      attr_accessor :page, :locale
+
+      # TODO: In the future, pick the template to render using a custom
+      # resolver. Then this can be included in all the public controllers,
+      # meaning they all get locale support for free.
+      def show
+        template = page.view
+        render :template => "pages/#{template}.#{locale.slug}.#{locale.code}"
+      end
+
+      private
+
+      def retrieve_page_and_locale
+        @page = env['gluttonberg.page']
+        @locale = env['gluttonberg.locale']
+      end
+    end
+  end
+end
+
