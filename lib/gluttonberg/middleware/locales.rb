bdbd3f8b85190bce44ff3560d14efbefac8077bb
Abdul <rauf_eng@hotmail.com>
2011-04-07 15:56:52 +0930

    Put Conditions for stylesheets,javascripts,images Also it will execute dynamic code for static resources

--- lib/gluttonberg/middleware/locales.rb
+++ lib/gluttonberg/middleware/locales.rb
@@ -6,19 +6,22 @@ module Gluttonberg
       end
 
       def call(env)
-        case Gluttonberg::Engine.config.identify_locale
-          when :subdomain
-            # return the sub-domain
-          when :prefix
-            locale, dialect = env['PATH_INFO'].split('/')[1..2]
-            result = Gluttonberg::Locale.find_by_locale(locale, dialect)
-            if result
-              env['PATH_INFO'].gsub!("/#{locale}/#{dialect}", '')
-              env['gluttonberg.locale'] = result
-            end
-          when :domain
-            env['SERVER_NAME']
-        end
+        path = env['PATH_INFO']
+        unless path =~ /^#{Gluttonberg::Engine.config.admin_path}/ || path.start_with?("/stylesheets")  || path.start_with?("/javascripts")   || path.start_with?("/images") 
+          case Gluttonberg::Engine.config.identify_locale
+            when :subdomain
+              # return the sub-domain
+            when :prefix
+              locale, dialect = path.split('/')[1..2]
+              result = Gluttonberg::Locale.find_by_locale(locale, dialect)
+              if result
+                env['PATH_INFO'].gsub!("/#{locale}/#{dialect}", '')
+                env['gluttonberg.locale'] = result
+              end
+            when :domain
+              env['SERVER_NAME']
+          end
+        end  
         @app.call(env)
       end
     end # Locales
--- lib/gluttonberg/middleware/rewriter.rb
+++ lib/gluttonberg/middleware/rewriter.rb
@@ -7,10 +7,7 @@ module Gluttonberg
 
       def call(env)
         path = env['PATH_INFO']
-
-        puts env['gluttonberg.locale'].inspect
-
-        unless path =~ /^#{Gluttonberg::Engine.config.admin_path}/
+        unless path =~ /^#{Gluttonberg::Engine.config.admin_path}/ || path.start_with?("/stylesheets")  || path.start_with?("/javascripts")   || path.start_with?("/images") 
           page = Gluttonberg::Page.find_by_path(path, env['gluttonberg.locale'])
           if page
             env['gluttonberg.page'] = page
@@ -28,4 +25,3 @@ module Gluttonberg
     end # Rewriter
   end # Middleware
 end # Gluttonberg
-
