47946a04b7a80df7752fa44af7e235b5096efe97
Yuri Tomanek <yuri@tomanek.com.au>
2011-04-11 17:22:03 +0930

    custom content generator approx 75% complete.
    Signed-off-by: Yuri Tomanek <yuri@tomanek.com.au>

---
+++ lib/generators/gluttonberg/resource/templates/migration.rb
@@ -0,0 +1,16 @@
+class Create<%= class_name %> < ActiveRecord::Migration
+  
+  def self.up
+    create_table :<%= table_name %> do |t|
+  <% attributes.each do |attribute| -%>
+    t.<%= attribute.type %> :<%= attribute.name %>
+  <% end -%>
+    t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :<%= table_name %>
+  end
+  
+end
