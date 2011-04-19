734b9e1a9cd54ef5fcf64b903a02e3758b4b8d85
Abdul <rauf_eng@hotmail.com>
2011-04-20 09:08:48 +0930

    Do not create if locale already exist. Also include SlugManagement mixin

--- app/models/gluttonberg/locale.rb
+++ app/models/gluttonberg/locale.rb
@@ -1,11 +1,13 @@
 module Gluttonberg
   class Locale  < ActiveRecord::Base
+    include Content::SlugManagement
     set_table_name "gb_locales"
 
     has_many    :page_localizations,  :class_name => "Gluttonberg::PageLocalization" , :dependent => :destroy 
     
     validates_presence_of :name , :slug
-
+    validates_uniqueness_of :slug , :name  
+    
     SLUG_TYPES = ["prefix"]
 
     def  self.first_default(opts={})
@@ -23,6 +25,12 @@ module Gluttonberg
     
     def self.find_by_locale(locale_slug)
       find(:first , :conditions => { :slug => locale_slug } )
+    end
+    
+    def self.generate_default_locale
+      if Gluttonberg::Locale.find(:first , :conditions => {:slug => "en-au"})  
+        locale = Gluttonberg::Locale.create( :slug => "en-au" , :name => "Australia English" , :default => true , :slug_type => Gluttonberg::Locale.prefix_slug_type )            
+      end  
     end
   end
 end
