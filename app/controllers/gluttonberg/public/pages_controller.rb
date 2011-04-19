a6d522d5becae9ea15cd05b56b05135eefb67e43
Abdul <rauf_eng@hotmail.com>
2011-04-19 15:04:49 +0930

    do not render layout for ajax requests

--- app/controllers/gluttonberg/public/pages_controller.rb
+++ app/controllers/gluttonberg/public/pages_controller.rb
@@ -6,11 +6,18 @@ module Gluttonberg
       # If localized template file exist then render that file otherwise render non-localized template
       def show
         template = page.view
+        template_path = "pages/#{template}" 
+        
         if File.exists?(File.join(Rails.root,  "app/views/pages/#{template}.#{locale.slug}.html.haml" ) )
-          render :template => "pages/#{template}.#{locale.slug}"
-        else
-          render :template => "pages/#{template}" 
+          template_path = "pages/#{template}.#{locale.slug}"
         end  
+        
+        # do not render layout for ajax requests
+        if request.xhr?
+          render :template => template_path, :layout => false
+        else
+          render :template => template_path
+        end
       end
       
       private 
