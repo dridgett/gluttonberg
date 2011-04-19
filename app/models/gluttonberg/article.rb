b11a7eab91670b971a66c954c43ce57ae82494ab
Abdul <rauf_eng@hotmail.com>
2011-04-20 09:29:31 +0930

    Cleaning

--- app/models/gluttonberg/article.rb
+++ app/models/gluttonberg/article.rb
@@ -1,9 +1,9 @@
 module Gluttonberg
   class Article < ActiveRecord::Base
     set_table_name "gb_articles"
+    include Content::SlugManagement
     
     belongs_to :blog
-    include Content::SlugManagement
     belongs_to :author, :class_name => "User"
     has_many :comments, :as => :commentable, :dependent => :destroy
     
