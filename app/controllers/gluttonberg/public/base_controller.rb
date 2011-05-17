6eb2674f71ec70a5ea155a845595797bc7107fbe
Yuri Tomanek <yuri@tomanek.com.au>
2011-05-17 11:18:58 +0930

    current_user

--- app/controllers/gluttonberg/public/base_controller.rb
+++ app/controllers/gluttonberg/public/base_controller.rb
@@ -19,22 +19,63 @@ class Gluttonberg::Public::BaseController < ActionController::Base
     rescue_from ActionController::RoutingError, :with => :not_found
     
   protected
-  
-      def retrieve_locale
-        @locale = env['gluttonberg.locale']
-      end
-      
-      # Exception handlers
-       def not_found
-          render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/not_found'
-        end
+    
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
 
-        # handle NotAcceptable exceptions (406)
-        def not_acceptable
-          render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/not_acceptable'
-        end
-        def internal_server_error
-          render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/internal_server_error'
-        end
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
+    end
+
+    def require_user
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to admin_login_url
+        return false
+      end
+      true
+    end
+    
+    def require_super_admin_user
+      return false unless require_user
+      
+      unless current_user.super_admin?
+        store_location
+        flash[:notice] = "You dont have privilege to access this page"
+        redirect_to admin_login_url
+        return false
+      end
+    end
+    
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+    
+    def retrieve_locale
+      @locale = env['gluttonberg.locale']
+    end
+    
+    # Exception handlers
+    def not_found
+      render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/not_found'
+    end
+
+    # handle NotAcceptable exceptions (406)
+    def not_acceptable
+      render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/not_acceptable'
+    end
+    
+    def internal_server_error
+      render :layout => "bare" , :template => 'gluttonberg/admin/exceptions/internal_server_error'
+    end
 
 end
