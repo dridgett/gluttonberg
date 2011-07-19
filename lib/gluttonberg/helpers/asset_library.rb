44427e91bd0d0be31314104b2c72d07953bee853
Abdul <rauf_eng@hotmail.com>
2011-07-19 13:48:23 +0930

    image captions for photo gallery

--- lib/gluttonberg/helpers/asset_library.rb
+++ lib/gluttonberg/helpers/asset_library.rb
@@ -142,7 +142,12 @@ module Gluttonberg
          unless gallery.blank? || gallery.gallery_images.blank?
            options = "" 
            gallery.gallery_images.each do |g_image|
-             options << content_tag(:li , link_to(asset_tag(g_image.image , gallery_thumb_image).html_safe , asset_url(g_image.image , :thumb_name => gallery_large_image) , :class => "thumb") , :id => "image_#{g_image.id}").html_safe
+             li_html = link_to(asset_tag(g_image.image , gallery_thumb_image).html_safe , asset_url(g_image.image , :thumb_name => gallery_large_image) , :class => "thumb") 
+             unless g_image.image.alt.blank?
+               image_desc_html = content_tag(:div , g_image.image.alt  , :class => "image-desc")
+               li_html << content_tag(:div , image_desc_html , :class => "caption")
+             end 
+             options << content_tag(:li , li_html , :id => "image_#{g_image.id}").html_safe
            end
            content_tag(:ul  , options.html_safe , html_opts_for_ul)
          end
